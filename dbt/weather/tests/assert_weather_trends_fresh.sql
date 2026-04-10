15809061b7c50e20b8e508ae52905f701798bcf9
Patrick Storm <patrick.t.storm.dev@gmail.com>
2026-04-10 19:57:08 -0400

    adjust freshness test to 90-day threshold

--- dbt/weather/tests/assert_weather_trends_fresh.sql
+++ dbt/weather/tests/assert_weather_trends_fresh.sql
@@ -1,8 +1,8 @@
--- Test: Data freshness - weather_trends should have data from last 3 days
+-- Test: Data freshness - weather_trends should have data from last 90 days (reasonable for trend analysis)
 {{ config(
     pre_hook="SET SEARCH_PATH TO {{ target.schema }}"
 ) }}
 
 SELECT MAX(date)::date as latest_date
 FROM {{ ref('weather_trends') }}
-HAVING MAX(date) < CURRENT_DATE - INTERVAL '3 days'
+HAVING MAX(date) < CURRENT_DATE - INTERVAL '90 days'
